076848ebe6229812ed3ce54bd809716db1d3be1a
Кухоцкая Екатерина <ekukhotskaya@mail.ru>
2022-05-29 18:04:34 +0300

    Reformat code

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
+        double sumCredit = 1_000_000;
+        double interestRate = 9.99;
+        int creditPeriod;
+
+        creditPeriod = 1;
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 руб., процентная ставка 9.99%, срок кредита 1 год");
-        System.out.println("Аннуитетный платеж: " + service.calculate(1_000_000,9.99,1) + " руб.");
+        System.out.println("Сумма кредита: " + sumCredit + " руб., процентная ставка: " + interestRate + "%, срок кредита: " + creditPeriod + " год");
+        System.out.println("Аннуитетный платеж: " + service.calculate(sumCredit, interestRate, creditPeriod) + " руб.");
+
+        creditPeriod = 2;
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 руб., процентная ставка 9.99%, срок кредита 2 года");
-        System.out.println("Аннуитетный платеж: " + service.calculate(1_000_000,9.99,2) + " руб.");
+        System.out.println("Сумма кредита: " + sumCredit + " руб., процентная ставка: " + interestRate + "%, срок кредита: " + creditPeriod + " года");
+        System.out.println("Аннуитетный платеж: " + service.calculate(sumCredit, interestRate, creditPeriod) + " руб.");
+
+        creditPeriod = 3;
         System.out.println();
-        System.out.println("Сумма кредита: 1 000 000 руб., процентная ставка 9.99%, срок кредита 3 года");
-        System.out.println("Аннуитетный платеж: " + service.calculate(1_000_000,9.99,3) + " руб.");
+        System.out.println("Сумма кредита: " + sumCredit + " руб., процентная ставка: " + interestRate + "%, срок кредита: " + creditPeriod + " года");
+        System.out.println("Аннуитетный платеж: " + service.calculate(sumCredit, interestRate, creditPeriod) + " руб.");
     }
 }
